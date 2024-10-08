4b0e0211a3fea35175faf2adaa98d432c5b53455
Stakd1 <42050379+Stakd1@users.noreply.github.com>
2024-10-08 17:07:34 +0800

    Update vue.config.js

--- newspaper/vue.config.js
+++ newspaper/vue.config.js
@@ -8,4 +8,19 @@ module.exports = {
       },
     }
   }
+  const service = axios.create({
+
+  baseURL:
+
+   process.env.NODE_ENV === 'dev'
+
+      ? process.env.VUE_APP_DEV_API
+
+      : process.env.VUE_APP_PRODUCTION_API,
+
+  timeout: 10000
+
+})
 }
+
+
